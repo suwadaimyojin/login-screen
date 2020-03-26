5371703145fb396d66dfea89a775ab3267a54975
suwadaimyojin <248975844@qq.com>
2020-03-26 14:15:23 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css');
 
-#nav {
-  padding: 30px;
+* {
+  margin: 0;
+  padding: 0;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+  text-decoration: inherit;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+ul,
+ol {
+  list-style: none;
+}
+input,
+select,
+button {
+  font: inherit;
+}
+html,
+body {
+  height: 100%;
+}
+body {
+  background-color: #007dff;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.app {
+  display: flex;
+  height: 100%;
+  align-items: center;
 }
 </style>
